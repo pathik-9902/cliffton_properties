ea099b114861d359acf8dbd8c3bf2dbbc01e0d56
pathik-9902 <pathik.patel992002@gmail.com>
2026-03-31 20:05:26 +0530

    Final

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY) || 'API_KEY_NOT_SET';
 
 export async function POST(req: Request) {
   try {
